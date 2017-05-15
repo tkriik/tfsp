b66eef3f1acc9730276c54cfeb3a579832ea1c2c
Tanel Kriik <tanel.kriik@outlook.com>
2017-05-15 20:10:11 +0300

    [fs_ent] fix sha256 spec

--- include/fs_ent.hrl
+++ include/fs_ent.hrl
@@ -1,6 +1,6 @@
 % File system entity metadata.
 -record(fs_ent, { path      :: file:path(),
-                  sha256    :: binary(),
+                  sha256    :: binary() | undefined,
                   size      :: non_neg_integer() | undefined,
                   type      :: regular | directory,
                   access    :: read | write | read_write | none | undefined,
